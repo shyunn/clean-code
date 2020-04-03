2560558cd6e4e8eae3a78121f282d56443691069
sunghyun yun <n87929@gmail.com>
2020-04-03 22:37:18 +0900

    4장 주석 리펙토링

--- src/me/shyun/cleancode/chapter4/PrimeGeneratorThreadSafe.java
+++ src/me/shyun/cleancode/chapter4/PrimeGeneratorThreadSafe.java
@@ -7,12 +7,10 @@ package me.shyun.cleancode.chapter4;
  * 처음으로 남아 있는 정수를 찾아 배수를 모두 제거한다.
  * 배열에 더이상 배수가 없을 때까지 반복한다.
  */
-public class PrimeGenerator {
+public class PrimeGeneratorThreadSafe {
 
   // thread Safe 하지 않다.
   // 때문에 리펙토링 하기전의 GeneratePrimes 를 완벽하게 대체하지 못한다.
-  private static boolean[] crossedOut;
-  private static int[] result;
 
   public static int[] generatePrimes(int maxValue) {
 
@@ -20,67 +18,64 @@ public class PrimeGenerator {
       return new int[0];
     } else {
 
-      uncrossIntegerUpTo(maxValue);
-      crossedOutMultiples();
-      putUnCrossedIntegersIntoResult();
+      boolean[] crossedOut = uncrossIntegerUpTo(maxValue);
+      crossedOutMultiples(crossedOut);
+      return putUnCrossedIntegersIntoResult(crossedOut);
 
-      return result;
     }
   }
 
-  private static void uncrossIntegerUpTo(int maxValue) {
+  private static boolean[] uncrossIntegerUpTo(int maxValue) {
 
-    crossedOut = new boolean[maxValue + 1];
+    boolean[] crossedOut = new boolean[maxValue + 1];
     for (int i = 2; i < crossedOut.length; i++) {
       crossedOut[i] = false;
     }
 
+    return crossedOut;
   }
 
-  private static void crossedOutMultiples() {
+  private static void crossedOutMultiples(boolean[] crossedOut) {
 
-    int limit = determineIterationLimit();
+    int limit = determineIterationLimit(crossedOut.length);
     for (int i = 2; i <= limit; i++) {
-      if (notCrossed(i)) {
-        crossOutMultipleOf(i);
+      if (!crossedOut[i]) { // notCrossed
+        crossOutMultipleOf(crossedOut,i);
       }
-
     }
   }
 
-  private static int determineIterationLimit() {
+  private static int determineIterationLimit(int length) {
 
     // 배열에 있는 모든 수는 배열 크기의 제곱근 보다 작은 소수의 인수다
     // 따라서 이 제곱근보다 더 큰 숫자의 배수는 제거할 필요가 없다.
-    double iterationLimit = Math.sqrt(crossedOut.length);
+    double iterationLimit = Math.sqrt(length);
     return (int) iterationLimit;
   }
 
-  private static boolean notCrossed(int i) {
-    return !crossedOut[i];
-  }
-
-  private static void crossOutMultipleOf(int i) {
+  private static void crossOutMultipleOf(boolean[] crossedOut, int i) {
     for (int multiple = 2 * i; multiple < crossedOut.length; multiple += i) {
       crossedOut[multiple] = true;
     }
   }
 
 
-  private static void putUnCrossedIntegersIntoResult() {
-    result = new int[numberOfUncrossedIntegers()];
+  private static int[] putUnCrossedIntegersIntoResult(boolean[] crossedOut) {
+
+    int[] result = new int[numberOfUncrossedIntegers(crossedOut)];
     for (int i = 0 ,j = 2; j < crossedOut.length; j++) {
-      if(notCrossed(j)){
+      if(!crossedOut[j]){ // notCrossed
         result[i++]=j;
       }
     }
+    return result;
   }
 
-  private static int numberOfUncrossedIntegers() {
+  private static int numberOfUncrossedIntegers(boolean[] crossedOut) {
     int count = 0;
 
     for (int i = 2; i < crossedOut.length; i++) {
-      if(notCrossed(i)){
+      if(!crossedOut[i]){ // notCrossed
         count++;
       }
 
